fb35f5e09c28859e7e63ec4006e6b249bcdb8044
KuzakinSemyon <kuzakinsemen@gmail.com>
2025-09-12 21:22:33 +0500

    Task4

---
+++ Tyuiu.KuzakinSI.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KuzakinSI.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KuzakinSI.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95264085-a357-48ec-943b-1e2329197983")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KuzakinSI.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KuzakinSI.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KuzakinSI.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5, 5));
+            Console.WriteLine(DataService.Substraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(5, 5));
+            Console.WriteLine(DataService.Division(5, 5));
+        }
+    }
+}
